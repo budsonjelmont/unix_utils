06e29b014a6f8c302cb64367fc6bbd3fdea049bf
jmb <judson.belmont@mssm.edu>
2020-11-28 23:30:57 -0500

    Initialize repo

---
+++ alerts/monitor_df.sh
@@ -0,0 +1,18 @@
+# Check available space on a partition (arg1), and if it is greater than X% (arg2), send an email to the server admin (arg3) 
+
+part=$1 # Must exactly match column 1 of the df output. E.g. /dev/mapper/vg01-lv.var
+alert_thresh=$2 # e.g. 90
+admin=$3 # e.g. judson.belmont@mssm.edu
+
+server=$(hostname -A) # could use nslookup [ip_addr] instead to full domain name. This may not work on the pi...
+
+# Get df -h output and parse it to get the percentage 
+use_perc=$(df -h $part | tail -n1 | tr -s ' ' | cut -d' ' -f5)
+use_perc_rstrip=${use_perc%*%}
+
+# If percentage used is greater than the alert threshold, send email 
+if [ $use_perc_rstrip -gt $alert_thresh ]
+then
+  echo "Disk is $use_perc full. Sending alert to $admin..."
+  echo -e "Subject:$part on $server is $use_perc_rstrip% full\nPartition $part on $server is $use_perc_rstrip% full. You should delete some files." | sendmail $admin
+fi
---
+++ bash_init/.bash_aliases
@@ -0,0 +1,29 @@
+# This file is sourced from .bashrc & contains all my aliases 
+
+# ls aliases
+alias ll='ls -alFh'
+alias la='ls -Ah'
+alias l='ls -althr'
+
+# commands
+alias q=’exit’
+alias c=’clear’
+alias h=’history’
+alias d=’pwd’
+alias t=’time’
+alias k='kill'
+alias null=’/dev/null’
+
+# fast navigation
+alias home='cd ~'
+alias root='cd /'
+alias dtop='cd ~/Desktop'
+
+# git commands
+alias g=’git’
+alias gs=’git status’
+alias ga=’git add’
+alias com=’git commit -m’
+alias clone=’git clone’
+alias sth=’git stash’
+alias gl=’git log’
